e3338cbd07a1cd083cd87ae714e2fba067d24f39
Afonso <67978137+AfonsoPaula@users.noreply.github.com>
2024-03-20 15:25:35 +0100

    App1 changes / Create environment for app2

---
+++ Data_Science/Streamlit/app_02_simple_bioinformatics_dna/dna-app.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import streamlit as st
+import altair as alt
+
+# Page title
+st.write("""
+# DNA Nucleotide Count Web App
+
+This app counts the nucleotide composition of query DNA!
+
+***
+""")
+
+
+# Input Text Box
+
+#st.sidebar.header('Enter DNA sequence')
+st.header('Enter DNA sequence')
+
+sequence_input = ">DNA Query 2\nGAACACGTGGAGGCAAACAGGAAGGTGAAGAAGAACTTATCCTATCAGGACGGAAGGTCCTGTGCTCGGG\nATCTTCCAGACGTCGCGACTCTAAATTGCCCCCTCTGAGGTCAAGGAACACAAGATGGTTTTGGAAATGC\nTGAACCCGATACATTATAACATCACCAGCATCGTGCCTGAAGCCATGCCTGCTGCCACCATGCCAGTCCT"
